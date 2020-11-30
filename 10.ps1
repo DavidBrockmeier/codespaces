b89a2237f254a3e27c2b13a1eee8eb39c2f9ea59
David Brockmeier <David@GeniusClick.com>
2020-11-30 05:32:54 +0000

    EnableAdminShares

--- 10.ps1
+++ 10.ps1
@@ -2,3 +2,4 @@ rd -Force -Recurse "C:\Program Files\WindowsPowerShell\Modules\Win10\"
 mkdir -f "C:\Program Files\WindowsPowerShell\Modules\Win10\"
 iwr https://raw.githubusercontent.com/Disassembler0/Win10-Initial-Setup-Script/master/Win10.psm1 -OutFile "C:\Program Files\WindowsPowerShell\Modules\Win10\Win10.psm1"
 EnableIPv6
+EnableAdminShares
