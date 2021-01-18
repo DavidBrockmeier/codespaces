80075ad425a066c61e2580566f874c0ece270e28
David Brockmeier <34142212+DavidBrockmeier@users.noreply.github.com>
2021-01-18 14:24:02 -0800

    update

---
+++ quon.ps1
@@ -0,0 +1,22 @@
+net use S: \\192.168.1.24\Scans /persistent:yes
+# rd -Force -Recurse "C:\Program Files\WindowsPowerShell\Modules\Win10\"
+# mkdir -f "C:\Program Files\WindowsPowerShell\Modules\Win10\"
+# #iwr -useb https://raw.githubusercontent.com/Ad3t0/windows/master/powershell-core/RDPWrapperInstall.ps1 | iex
+# iwr https://raw.githubusercontent.com/Disassembler0/Win10-Initial-Setup-Script/master/Win10.psm1 -OutFile "C:\Program Files\WindowsPowerShell\Modules\Win10\Win10.psm1"
+# Import-Module "C:\Program Files\WindowsPowerShell\Modules\Win10\Win10.psm1"
+# DisableDiagTrack
+# DisableErrorReporting
+# DisableFastStartup
+# DisableTailoredExperiences
+# DisableTelemetry
+# DisableWebSearch
+# EnableIPv6
+# EnableIPv6
+# EnableLLDP
+# EnableLLMNR
+# EnableLLTD
+# EnableNTFSLongPaths
+# EnableNTFSLongPaths
+# SetCurrentNetworkPrivate
+# SetUnknownNetworksPrivate
+# UninstallThirdPartyBloat
