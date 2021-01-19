7c6144dc9c55e5e55e93c12fcf52cb8ef3af3eab
David Brockmeier <34142212+DavidBrockmeier@users.noreply.github.com>
2021-01-19 08:12:35 -0800

    Update quon.ps1

--- quon.ps1
+++ quon.ps1
@@ -1,4 +1,12 @@
-rd -Force -Recurse "C:\Program Files\WindowsPowerShell\Modules\Win10\"
+$ErrorActionPreference = "SilentlyContinue"
+
+# EnableLLDP;EnableLLMNR;EnableLLTD;EnableNetDevicesAutoInst;DisableHomeGroups;EnableNetBIOS;EnableIPv6;EnableRemoteDesktop;DisableMediaSharing
+# Get-Service fdPHost,FDResPub | Set-Service -StartupType Automatic | Start-Service
+# Get-NetAdapter | ? MediaConnectionState -eq Connected | Set-NetAdapterBinding -Enabled $true -ComponentID ms_lldp,ms_lltdio,ms_rspndr,ms_tcpip6
+# New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\DNSClient" -Force | Out-Null
+# Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\DNSClient" -Name "EnableMulticast" -Type DWord -Value 1
+
+rd -Force -Recurse "C:\Program Files\WindowsPowerShell\Modules\Win10\" 
 mkdir -f "C:\Program Files\WindowsPowerShell\Modules\Win10\"
 #iwr -useb https://raw.githubusercontent.com/Ad3t0/windows/master/powershell-core/RDPWrapperInstall.ps1 | iex
 iwr https://raw.githubusercontent.com/Disassembler0/Win10-Initial-Setup-Script/master/Win10.psm1 -OutFile "C:\Program Files\WindowsPowerShell\Modules\Win10\Win10.psm1"
@@ -45,3 +53,6 @@ SetUnknownNetworksPrivate
 
 net use S: /delete
 net use S: \\192.168.1.24\Scans /persistent:yes /user:guest
+
+Enable-PSRemoting
+winrm.cmd quickconfig -q
