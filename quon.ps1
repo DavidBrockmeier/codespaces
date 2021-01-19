2cbd15c8e3d62f38d56f562f00b80346d0e0747c
David Brockmeier <34142212+DavidBrockmeier@users.noreply.github.com>
2021-01-19 07:34:53 -0800

    fix network

--- quon.ps1
+++ quon.ps1
@@ -1,23 +1,47 @@
-net use S: /delete
-net use S: \\192.168.1.24\Scans /persistent:yes
-# rd -Force -Recurse "C:\Program Files\WindowsPowerShell\Modules\Win10\"
-# mkdir -f "C:\Program Files\WindowsPowerShell\Modules\Win10\"
-# #iwr -useb https://raw.githubusercontent.com/Ad3t0/windows/master/powershell-core/RDPWrapperInstall.ps1 | iex
-# iwr https://raw.githubusercontent.com/Disassembler0/Win10-Initial-Setup-Script/master/Win10.psm1 -OutFile "C:\Program Files\WindowsPowerShell\Modules\Win10\Win10.psm1"
-# Import-Module "C:\Program Files\WindowsPowerShell\Modules\Win10\Win10.psm1"
-# DisableDiagTrack
-# DisableErrorReporting
-# DisableFastStartup
-# DisableTailoredExperiences
-# DisableTelemetry
-# DisableWebSearch
-# EnableIPv6
-# EnableIPv6
-# EnableLLDP
-# EnableLLMNR
-# EnableLLTD
-# EnableNTFSLongPaths
-# EnableNTFSLongPaths
-# SetCurrentNetworkPrivate
-# SetUnknownNetworksPrivate
+rd -Force -Recurse "C:\Program Files\WindowsPowerShell\Modules\Win10\"
+mkdir -f "C:\Program Files\WindowsPowerShell\Modules\Win10\"
+#iwr -useb https://raw.githubusercontent.com/Ad3t0/windows/master/powershell-core/RDPWrapperInstall.ps1 | iex
+iwr https://raw.githubusercontent.com/Disassembler0/Win10-Initial-Setup-Script/master/Win10.psm1 -OutFile "C:\Program Files\WindowsPowerShell\Modules\Win10\Win10.psm1"
+Import-Module "C:\Program Files\WindowsPowerShell\Modules\Win10\Win10.psm1"
+
+# DisableAutoplay
+# DisableAutorun
+# DisableCortana
+# DisableFeedback
 # UninstallThirdPartyBloat
+DisableActivityHistory
+DisableAdvertisingID
+DisableAppSuggestions
+DisableDiagTrack
+DisableErrorReporting
+DisableFastStartup
+DisableMaintenanceWakeUp
+DisableSharedExperiences
+DisableTailoredExperiences
+DisableTelemetry
+DisableTelemetry
+DisableUpdateAutoDownload
+DisableUpdateRestart
+DisableUWPBackgroundApps
+DisableWAPPush
+DisableWebSearch
+DisableWiFiSense
+EnableAdminShares
+EnableDotNetStrongCrypto
+EnableF8BootMenu
+EnableHibernation
+EnableIPv6
+EnableLLDP
+EnableLLMNR
+EnableLLTD
+EnableNetBIOS
+EnableNTFSLongPaths
+EnableScriptHost
+EnableUpdateMSProducts
+HideAccountProtectionWarn
+SetCurrentNetworkPrivate
+SetP2PUpdateLocal
+SetUnknownNetworksPrivate
+
+net use S: /delete
+net use S: \\192.168.1.24\Scans /persistent:yes /user:guest
